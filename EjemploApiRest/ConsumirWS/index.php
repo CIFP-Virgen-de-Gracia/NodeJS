3a3d47997d3a98f2531ea64fcd5ed9260515e1fe
aranzabe <faranzabe@gmail.com>
2021-02-15 11:21:56 +0100

    API Rest sin token - Febrero 2021

--- EjemploApiRest/ConsumirWS/index.php
+++ EjemploApiRest/ConsumirWS/index.php
@@ -16,39 +16,39 @@ Consumimos un servicio creado en Java Rest http://localhost:8080/EjemploGF/app/S
 //        //establecemos el verbo http que queremos utilizar para la petición
 //        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 //        $response = curl_exec($ch);
-        
 //      //------------------------------------------------------------------------------------------
         //Haciendo una petición GET con parámetro en la URL.
-        $ch = curl_init("http://localhost:8090/personas/999");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        //establecemos el verbo http que queremos utilizar para la petición
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        $response = curl_exec($ch);
-        
+//        $ch = curl_init("http://localhost:8090/personas/90B");
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//        //establecemos el verbo http que queremos utilizar para la petición
+//        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+//        $response = curl_exec($ch);
         
         //----------- Código común para los GET de arriba ---------
         // Se cierra el recurso CURL y se liberan los recursos del sistema
-        curl_close($ch);
-        if (!$response) {
-            return false;
-        } else {
-            echo '<br>';
-            var_dump($response);
-            $data = json_decode($response, true);
-            if (empty($data)) {
-                echo 'No se han obtenido registros.';
-            } else {
-                foreach ($data as $cod => $emp) {
-                    echo 'Nombre: ' . $emp['nombre'] . '<br>Ciudad: ' . $emp['ciudad'] . '<br>--------<br>';
-                }
-            }
-        }
+//        curl_close($ch);
+//        if (!$response) {
+//            return false;
+//        } else {
+//            echo '<br>';
+//            var_dump($response);
+//            $data = json_decode($response, true);
+//            if (empty($data)) {
+//                echo 'No se han obtenido registros.';
+//            } else {
+//                foreach ($data as $cod => $emp) {
+//                    echo 'DNI: '. $emp['DNI'].'<br>Nombre: ' . $emp['Nombre'] . '<br>Tfno: ' . $emp['Tfno'] . '<br>--------<br>';
+//                }
+//            }
+//        }
+
         
         
 //      //------------------------------------------------------------------------------------------
         //Haciendo una petición POST: para insertar un artículo.        
-//        $data = ['nombre' => 'Nuevo nombre',
-//            'ciudad' => 'Miguelturra de Cva'];
+//        $data = ['dni' => '9091B',
+//            'nombre' => 'Un alumno',
+//            'tfno' => '123456789'];
 //        $datos = json_encode($data);
 ////        var_dump($datos);
 //        $ch = curl_init("http://localhost:8090/personas");
@@ -66,14 +66,15 @@ Consumimos un servicio creado en Java Rest http://localhost:8080/EjemploGF/app/S
 //        } else {
 //            echo $response;
 //        }
-        
-        
+
+
 //      //------------------------------------------------------------------------------------------
         //Haciendo una petición PUT: para modificar un artículo.        
-//        $data = ['nombre' => 'Ya acabo',
-//            'ciudad' => 'Menuda turra'];
+//        $data = [
+//            'Nombre' => 'Otro alumno',
+//            'Tfno' => '12389'];
 //        $datos = json_encode($data);
-//        $ch = curl_init("http://localhost:8090/personas/23");
+//        $ch = curl_init("http://localhost:8090/personas/504");
 //        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 //        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 //        curl_setopt($ch, CURLOPT_POSTFIELDS, $datos);
@@ -91,8 +92,8 @@ Consumimos un servicio creado en Java Rest http://localhost:8080/EjemploGF/app/S
 
         
 //       //------------------------------------------------------------------------------------------
-//          //Haciendo una petición DELETE: para borrar un artículo.        
-//        $ch = curl_init("http://localhost:8090/personas/20");
+//       //Haciendo una petición DELETE: para borrar un artículo.        
+//        $ch = curl_init("http://localhost:8090/personas/909B");
 //        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 //        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
 //        $response = curl_exec($ch);
